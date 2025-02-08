5892c57c38a0d21b8d93f2672211bf47c8441cd8
Anjali Gautam <gautamanjali569@gmail.com>
2025-02-09 03:37:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 
+package main
+
+import (
+	"fmt"
+	"os"
+	"pqc-tendermint-benchmark/benchmarks"
+)
+
+func main() {
+	fmt.Println("Starting Dilithium vs. Ed25519 Benchmarking...")
+	
+	benchmarks.RunBenchmarks()
+	
+	fmt.Println("Benchmarking Completed. Check results directory for logs.")
+}
